a8c00c01159c88b7c27728331e352c7dfa5d9fc9
kailehok <baral.kailash12345@gmail.com>
2019-11-06 11:03:36 +0545

    changes in steem node

--- posts/services.py
+++ posts/services.py
@@ -4,11 +4,13 @@ from beem.blockchain import Blockchain
 from beem.account import Account
 from beem.comment import Comment
 from beem import Steem
+from beem.nodelist import NodeList
+nodelist = NodeList()
 
 
 # retrieve account blog history
 def account_history(username):
-    stm = Steem("https://steemd.minnowsupportproject.org")
+    stm = Steem(node=nodelist .get_nodes())
     account = Account(username, steem_instance=stm)
 
     return account.blog_history(limit=25, reblogs=False)
